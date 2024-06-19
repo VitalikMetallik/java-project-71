b205de0a7494cac62ad9a72fe1c99e60f611ace4
VitalikMetallik <flash_PS4@mail.ru>
2024-06-19 20:59:20 +0300

    3rd step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,5 +1,10 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
