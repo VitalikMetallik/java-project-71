f6e68a4e89cf123a63a571cd8d12a5cdb6bc8e4f
VitalikMetallik <flash_PS4@mail.ru>
2024-08-20 21:19:12 +0300

    Check

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,38 +2,36 @@ package hexlet.code;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
+import picocli.CommandLine.Option;
 
 import java.util.concurrent.Callable;
 
 @Command(name = "gendiff",
         mixinStandardHelpOptions = true,
+        version = "gendiff 0.1",
         description = "Compares two configuration files and shows a difference.")
-public class App implements Callable {
-    @Parameters(description = "path to first file")
+
+public class App implements Callable<String> {
+
+    @Parameters(index = "0", description = "path to first file")
     private String filepath1;
-    @Parameters(description = "path to second file")
+
+    @Parameters(index = "1", description = "path to second file")
     private String filepath2;
-    @Option(names = {"-f", "--format"},
-            description = "output format [default: stylish]",
-            defaultValue = "stylish")
+
+    @Option(names = {"-f", "--format"}, description = "output format [default: stylish]")
     private String format = "stylish";
 
-    @Override
-    public String call() {
-        String resultDiff = null;
-        try {
-            resultDiff = Differ.generate(filepath1, filepath2);
-            System.out.println(resultDiff);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        return resultDiff;
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 
-    public static void main(String[] args) {
-        CommandLine commandLine = new CommandLine(new App());
-        System.exit(commandLine.execute(args));
+    @Override
+    public final String call() throws Exception {
+        var diff = Differ.generate(filepath1, filepath2, format);
+        System.out.println(diff);
+        return diff;
     }
 }
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,47 +1,33 @@
 package hexlet.code;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import java.io.File;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.Map;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static hexlet.code.Comparator.compare;
+import static hexlet.code.Parser.parse;
 
 public class Differ {
-    private static Map<String, Object> getData(File json) throws IOException {
-        return new ObjectMapper().readValue(json, new TypeReference<Map<String, Object>>() {
-        });
+    public static String generate(String filePath1, String filePath2, String format) throws IOException {
+        Path firstPath = Paths.get(filePath1).toAbsolutePath();
+        Path secondPath = Paths.get(filePath2).toAbsolutePath();
+        var firstParsedMap = parse(readFile(firstPath), checkFileFormat(firstPath));
+        var secondParsedMap = parse(readFile(secondPath), checkFileFormat(secondPath));
+        var finalMap = compare(firstParsedMap, secondParsedMap);
+        return Formatter.format(finalMap, format);
     }
 
     public static String generate(String filePath1, String filePath2) throws IOException {
-        File file1 = new File(filePath1);
-        File file2 = new File(filePath2);
-        Map<String, Object> fileMap1 = getData(file1);
-        Map<String, Object> fileMap2 = getData(file2);
-        if (fileMap1.isEmpty() && fileMap2.isEmpty()) {
-            return "{}";
-        }
-        SortedSet<String> keySet = Collections.synchronizedSortedSet(new TreeSet<>());
-        fileMap1.forEach((k, v) -> keySet.add(k));
-        fileMap2.forEach((k, v) -> keySet.add(k));
-        StringBuilder result = new StringBuilder("{");
-        keySet.forEach(key -> {
-            if (fileMap1.containsKey(key) && fileMap2.containsKey(key)) {
-                if (fileMap2.get(key).equals(fileMap1.get(key))) {
-                    result.append("\n    ").append(key).append(":").append(fileMap1.get(key));
-                } else {
-                    result.append("\n  - ").append(key).append(":").append(fileMap1.get(key));
-                    result.append("\n  + ").append(key).append(":").append(fileMap2.get(key));
-                }
-            } else if (fileMap1.containsKey(key) && !fileMap2.containsKey(key)) {
-                result.append("\n  - ").append(key).append(":").append(fileMap1.get(key));
-            } else {
-                result.append("\n  + ").append(key).append(":").append(fileMap2.get(key));
-            }
-        });
-        return result.append("\n}").toString();
+        return generate(filePath1, filePath2, "stylish");
+    }
+
+    public static String readFile(Path filePath) throws IOException {
+        return Files.readString(filePath);
+    }
+
+    public static String checkFileFormat(Path filePath) {
+        var pathString = filePath.toString();
+        return pathString.substring(pathString.lastIndexOf(".") + 1);
     }
 }
